6b6962fb4f9c0c4fd5178f292807bf50296e9f87
manzi osee <oseemanzi3@gmail.com>
2022-06-26 22:30:22 +0200

    commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,22 +1,21 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.timezone import now
- 
+
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     image = models.ImageField(upload_to="profile_pics", blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+    phone_no = models.IntegerField(blank=True, null=True)
     facebook = models.CharField(max_length=300, blank=True, null=True)
     instagram = models.CharField(max_length=300, blank=True, null=True)
     linkedin = models.CharField(max_length=300, blank=True, null=True)
     
     def __str__(self):
         return str(self.user)
- 
+
 class BlogPost(models.Model):
     title=models.CharField(max_length=255)
     author= models.ForeignKey(User, on_delete=models.CASCADE)
@@ -31,12 +30,15 @@ class BlogPost(models.Model):
     def get_absolute_url(self):
         return reverse('blogs')
     
+    
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
     parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)   
     dateTime=models.DateTimeField(default=now)
- 
+
     def __str__(self):
         return self.user.username +  " Comment: " + self.content
+    
+
